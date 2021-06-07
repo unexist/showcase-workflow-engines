287fe9a7a47596db87eaea5b98af100cb8aa9aae
Christoph Kappel <christoph@unexist.dev>
2021-06-07 12:00:30 +0200

    Added default todo service

---
+++ src/main/java/dev/unexist/showcase/todo/infrastructure/health/SimpleHealthCheck.java
@@ -0,0 +1,35 @@
+/**
+ * @package Quarkus-Camunda-Showcase
+ *
+ * @file Simple health check
+ * @copyright 2020-2021 Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the GNU GPLv3.
+ * See the file LICENSE for details.
+ **/
+
+package dev.unexist.showcase.todo.infrastructure.health;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@Liveness
+@ApplicationScoped
+public class SimpleHealthCheck implements HealthCheck {
+
+    /**
+     * Call of the healthcheck
+     *
+     * @return
+     *      Reponse of the healthcheck
+     **/
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.up("Simple health check");
+    }
+}
---
+++ src/test/java/dev/unexist/showcase/todo/application/TodoResourceTest.java
@@ -0,0 +1,29 @@
+/**
+ * @package Quarkus-Camunda-Showcase
+ *
+ * @file Stupid integration test
+ * @copyright 2020-2021 Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the GNU GPLv3.
+ * See the file LICENSE for details.
+ **/
+
+package dev.unexist.showcase.todo.application;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+public class TodoResourceTest {
+
+    @Test
+    public void testTodoEndpoint() {
+        given()
+          .when().get("/todo")
+          .then()
+             .statusCode(204);
+    }
+}
