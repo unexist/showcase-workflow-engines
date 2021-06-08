f9127f1fdc09708df198db0b431949d641a1ceeb
Christoph Kappel <christoph@unexist.dev>
2021-06-08 12:25:42 +0200

    Updated doc header

--- todo-service-camunda/src/main/java/dev/unexist/showcase/todo/infrastructure/camunda/CamundaEngineProvider.java
+++ todo-service-camunda/src/main/java/dev/unexist/showcase/todo/infrastructure/camunda/CamundaEngineProvider.java
@@ -1,3 +1,14 @@
+/**
+ * @package Quarkus-Workflow-Showcase
+ *
+ * @file Camunda engine provider
+ * @copyright 2021 Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the GNU GPLv3.
+ * See the file LICENSE for details.
+ **/
+
 package dev.unexist.showcase.todo.infrastructure.camunda;
 
 import org.camunda.bpm.engine.ProcessEngine;
