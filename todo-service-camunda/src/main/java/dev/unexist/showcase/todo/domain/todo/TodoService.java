f3ee531b7afff6b6149c79329dfecac247f6c1d4
Christoph Kappel <christoph@unexist.dev>
2021-08-11 14:33:35 +0200

    Updated logmessage and javadoc

--- todo-service-camunda/src/main/java/dev/unexist/showcase/todo/domain/todo/TodoService.java
+++ todo-service-camunda/src/main/java/dev/unexist/showcase/todo/domain/todo/TodoService.java
@@ -27,7 +27,7 @@ public class TodoService {
      *
      * @param  base  A {@link TodoBase} entry
      *
-     * @return
+     * @return Either {@code true} on success; otherwise {@code false}
      **/
 
     public boolean create(TodoBase base) {
